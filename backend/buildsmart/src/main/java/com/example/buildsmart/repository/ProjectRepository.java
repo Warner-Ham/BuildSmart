9f4b2e7e64a1ac35fb906d827d0d532f47d16849
IT24103193siyara <it24103193@my.sliit.lk>
2025-10-27 01:06:24 +0530

    repositories in the backend

--- backend/buildsmart/src/main/java/com/example/buildsmart/repository/ProjectRepository.java
+++ backend/buildsmart/src/main/java/com/example/buildsmart/repository/ProjectRepository.java
@@ -3,5 +3,8 @@ package com.example.buildsmart.repository;
 import com.example.buildsmart.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findByStatus(String inProgress);
 }
